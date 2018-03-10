9d86efaaaa7a626ecf4c58303d6b2ca3bb10a13f
gatchrat <henkolt@t-online.de>
2018-03-10 21:34:25 +0100

    label sizes dont depend on browser settings anymore

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -446,7 +446,14 @@ function setup() { // jshint ignore:line
 	
 	
     frameRate(60); // Caps the framerate at 60 FPS
-
+	
+	//sets font-size for all label elements
+	var labels  = document.getElementsByClassName('label');
+	for (i = 0; i < ps.length; i++) {
+		
+		ps[i].style.fontSize  = "16px";
+	}	
+	
     let loadfile = urlParam('sketch');
     if (loadfile !== "") {
         document.title = String(loadfile + ' - LogiJS');
