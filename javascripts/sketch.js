cd68b70f411326f0faff0283da33b46598744814
Christian Biasi <christianbiasi@gmx.de>
2018-08-18 23:55:00 +0200

    added validation when saving circuit names, whitspaces will be deleted

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -648,11 +648,23 @@ function customClicked(filename) {
 
 // Triggered when a sketch should be saved
 function saveClicked() {
-    selectMode = 'none';
-    showSClickBox = false;
-    previewSymbol = null;
-    saveSketch(textInput.value() + '.json');
-    document.title = textInput.value() + ' - LogiJS';
+    /*  
+        Only save circuits with "valid" names, here erasing all whitespaces (for now)
+        replace(...) used as suggestion from StackOverflow 
+        "/\s" says to sarch for the whitespace in the word (pattern),
+        "/g" says to search through the whole string and replace "\s" (whitespace) by ""
+        TODO: Delete all illegal characters
+    */
+    if(textInput.value().replace(/\s/g, "") != ""){
+        selectMode = 'none';
+        showSClickBox = false;
+        previewSymbol = null;
+        saveSketch(textInput.value() + '.json');
+        document.title = textInput.value() + ' - LogiJS';
+    } else {
+        // TODO: Error should pop up as tooltip under textInput
+        alert("Circuit name cannot be empty");
+    }
 }
 
 // Triggered when a sketch should be loaded
